6d2d04412337e84c35e64297572c0038d12f5283
dataking <dataking@gmail.com>
2015-10-22 11:57:08 -0700

    Working version.  Still WIP.

--- Proc.rb
+++ Proc.rb
@@ -13,86 +13,129 @@ class Proc::CpuInfo
 	attr_accessor :core_count
 	attr_accessor :cpus				# an array of Proc::CpuInfo::CPU objects
 
+	class Proc::CpuInfo::CPU
+		attr_accessor :processor_id
+		attr_accessor :vendor_id
+		attr_accessor :cpu_family
+		attr_accessor :model
+		attr_accessor :model_name
+		attr_accessor :stepping
+		attr_accessor :microcode
+		attr_accessor :cache_size
+		attr_accessor :cpu_mhz
+		attr_accessor :siblings
+		attr_accessor :core_id
+		attr_accessor :physical_id
+		attr_accessor :cpu_cores
+		attr_accessor :apic_id
+		attr_accessor :initial_acpi_id
+		attr_accessor :fdiv_bug
+		attr_accessor :f00f_bug
+		attr_accessor :coma_bug
+		attr_accessor :fpu
+		attr_accessor :fpu_exception
+		attr_accessor :cpuid_level
+		attr_accessor :wp
+		attr_accessor :flags
+		attr_accessor :bogomips
+		attr_accessor :clflush_size
+		attr_accessor :cachesize_alignment
+		attr_accessor :address_sizes
+		attr_accessor :power_management
+
+		def initialize() 
+			%x{/bin/cat /proc/cpuinfo}.split(/\n\n/).each do |g|
+				g.chomp!
+				g.to_s.split(/\n/).each do |l|
+					(name, value) = l.split(/\:/)
+					name.strip!
+					value.strip! unless value.nil? || value == ''
+
+					case name
+					when /processor/
+						@processor_id = value
+					when /vendor_id/
+						@vendor_id = value
+					when /cpu family/
+						@cpu_family = value
+					when /model/
+						@model = value
+					when /model name/
+						@model_name = value
+					when /stepping/
+						@stepping = value
+					when /microcode/
+						@microcode = value
+					when /cpu MHz/
+						@cpu_mhz = value
+					when /cache size/
+						@cache_size = value
+					when /physical id/
+						@physical_id = value
+					when /siblings/
+						@siblings = value
+					when /core id/
+						@core_id = value
+					when /cpu cores/
+						@cpu_cores = value
+					when /apicid/
+						@apicid = value
+					when /fdiv_bug/
+						@fdiv_bug = false if value == 'no'
+					when /f00f_bug/
+						@f00f_bug = false if value == 'no'
+					when /coma_bug/
+						@coma_bug = false if value == 'no'
+					when /fpu/
+						@fpu = true if value == 'yes'
+					when /cpuid level/
+						@cpuid_level = value
+					when /wp/
+						@wp = true if value == 'yes'
+					when /flags/
+						@flags = value.split(/ /) { |e| e.strip! }
+					when /bugs/
+						@bugs = value.split(/ /) { |e| e.strip! }
+					when /bogomips/
+						@bogomips = value
+					when /clflush size/
+						@clflush_size = value
+					when /cache_alignment/
+						@cache_alignment = value
+					when /address sizes/
+						@address_size = value.split(/\,/)
+						@address_size.each { |e| e.sub!(/\s*(.*?)/, "\\1") }
+					when /power management/
+						@power_management = value
+					else
+						raise "Unrecognized key-value pair:  #{name} : #{value}!"
+					end
+				end
+			end
+		end
+
+		def show_stuff
+			puts <<-EOS
+
+Processor		:	#{@processor_id}
+Vendor ID		:	#{@vendor_id}
+CPU Obj Count	:	#{@cpus.size.to_s}
+
+EOS
+		end
+	end
+
 	def initialize() 
 		@processor_count = %x{/bin/grep -c 'processor' /proc/cpuinfo}.chomp.strip.to_i
 		@core_count = %x{/bin/grep 'cpu cores' /proc/cpuinfo | /usr/bin/cut -d: -f2 | /usr/bin/sort -u}.chomp.strip.to_i
 
-		#if @processor_count > 1
-		#	cpu = Process::CpuInfo::CPU.new()
-		#	@cpus << cpu
-		#end
-	end
-end
-
-class Proc::CpuInfo::CPU
-	attr_accessor :processor_id
-	attr_accessor :vendor_id
-	attr_accessor :cpu_family
-	attr_accessor :model
-	attr_accessor :model_name
-	attr_accessor :stepping
-	attr_accessor :microcode
-	attr_accessor :cache_size
-	attr_accessor :cpu_mhz
-	attr_accessor :siblings
-	attr_accessor :core_id
-	attr_accessor :physical_id
-	attr_accessor :cpu_cores
-	attr_accessor :apic_id
-	attr_accessor :initial_acpi_id
-	attr_accessor :fdiv_bug
-	attr_accessor :f00f_bug
-	attr_accessor :coma_bug
-	attr_accessor :fpu
-	attr_accessor :fpu_exception
-	attr_accessor :cpuid_level
-	attr_accessor :wp
-	attr_accessor :flags
-	attr_accessor :bogomips
-	attr_accessor :clflush_size
-	attr_accessor :cachesize_alignment
-	attr_accessor :address_sizes
-	attr_accessor :power_management
-
-	def initialize() 
-		%x{"cat /proc/cpuinfo"}.split(/\n/).each do |l|
-			l.chomp!
-			(name, value) = l.split(/\:/)
-			name.strip!
-			value.strip!
-
-			case name
-			when /processor/
-				@processor_id = value
-			when /vendor_id/
-				@vendor_id = value
-			when /cpu family/
-				@cpu_family = value
-			when /model/
-				@model = value
-			when /model name/
-				@model_name = value
-			when /stepping/
-				@stepping = value
-			when /microsode/
-				@microcode = value
-			when /cpu MHz/
-				@cpu_mhz = value
-			when /cache size/
-				@cache_size = value
-			else
-				raise "Unrecognized key-value pair:  #{name} : #{value}!"
-			end
+		if @processor_count > 1
+			puts "Processing processors."
+			p = Proc::CpuInfo::CPU.new()
+			pp p
+			#@cpus << p
 		end
 	end
-
-	def pretty_print
-		puts <<-EOS
-
-Processor	:	#{@processor_id}
-Vendor ID	:	#{@vendor_id}
-
-EOS
-	end
 end
+
 
